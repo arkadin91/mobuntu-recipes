de00c9067541a5dcf7af118a3ac34ec8b66b53ef
Arkadin <dawidpiotrfidala@gmail.com>
2026-04-29 04:43:50 +0200

    Another hanges

--- scripts/final.sh
+++ scripts/final.sh
@@ -2,6 +2,9 @@
 
 set -x
 
+echo "Install firmware and kernel"
+dpkg -i --force-overwrite /opt/*.deb
+
 echo "Fix alsa-ucm-conf"
 wget https://repo.mobian.org/pool/main/a/alsa-ucm-conf/alsa-ucm-conf_1.2.15.3-1mobian3_all.deb
 dpkg -i --force-overwrite alsa-ucm-conf_1.2.15.3-1mobian3_all.deb
