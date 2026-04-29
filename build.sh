843d5e025ef5f976fbc75b7d32c8770bbd0e6196
Arkadin <dawidpiotrfidala@gmail.com>
2026-04-29 05:06:40 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,24 +3,6 @@
 export PATH=/sbin:/usr/sbin:$PATH
 IMG_FILE="mobuntu-sdm845-`date +%Y%m%d`.img"
 ARGS="--disable-fakemachine"
-username=
-password=
-
-while getopts "up" opt
-do
-  case "$opt" in
-    u ) username="$OPTARG" ;;
-    p ) password="$OPTARG" ;;
-  esac
-done
-
-if [ "$username" ]; then
-  ARGS="$ARGS -t username:\"$username\""
-fi
-
-if [ "$password" ]; then
-  ARGS="$ARGS -t password:\"$password\""
-fi
 
 ARGS="$ARGS --scratchsize=10G"
 
@@ -29,5 +11,3 @@ if [ ! "$image_only" ]; then
 fi
 debos $ARGS -t image:$IMG_FILE image.yaml
 
-echo "Compressing partitions $IMG_FILE..."
-gzip --keep --force $IMG_FILE
